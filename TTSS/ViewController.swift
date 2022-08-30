8ff7657b12b4bae03b64edd5911132c265155a01
fanxy_on <fanxy_on@alphacircle.co.kr>
2022-08-30 17:50:09 +0900

    Initial Commit

---
+++ TTSS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TTSS
+//
+//  Created by 박시온 on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
